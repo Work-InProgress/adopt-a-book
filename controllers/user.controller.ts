59d15e80c4855694c65fd5413d7757d2866898d4
afisman <afisman@gmail.com>
2024-06-27 16:06:35 +0200

    worked on user controller

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,9 +1,14 @@
 import express, { NextFunction, Request, Response } from 'express';
+import { fetchAllUsers } from '../services/user';
 
 export const userController = express.Router();
 
 userController.get('/', async (_req: Request, res: Response, next: NextFunction) => {
-
+    try {
+        const users = await fetchAllUsers();
+    } catch (error) {
+        next(error)
+    }
 })
 
 userController.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
